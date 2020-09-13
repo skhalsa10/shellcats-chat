ed56c7f003fc3665f16b9921e936142483fdddd5
Soheila <sjafarikhouzani@cs.unm.edu>
2020-09-12 23:27:53 -0600

    My first commit

--- src/Chat/ChatServer/ChatServer.java
+++ src/Chat/ChatServer/ChatServer.java
@@ -2,6 +2,8 @@ package Chat.ChatServer;
 
 import Chat.Messages.Message;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -10,19 +12,42 @@ import java.util.concurrent.LinkedBlockingQueue;
  * This class is a simple server class it brokers clients connections  and then forwards messages to them
  */
 public class ChatServer implements Runnable {
-    private Socket serverSocket;
+    private ServerSocket serverSocket;
     private ConcurrentHashMap<String,ClientConnection> clients;
     private LinkedBlockingQueue<Message> serverMessageQ;
     private String serverHostname;
     private int serverport;
     private ChatServerListener chatServerListener;
 
-    public ChatServer(String serverHostname, int serverport){
+    public ChatServer(String serverHostname, int serverport,LinkedBlockingQueue<Message> serverMessageQ,ConcurrentHashMap<String,ClientConnection> clients){
         //TODO initialize everything. start the chatserver listener, and then start the chatserver thread.
+        this. serverHostname=serverHostname;
+        this.serverport = serverport;
+        this.serverMessageQ=serverMessageQ;
+        this.clients=clients;
+        try {
+            this.serverSocket=new ServerSocket(serverport);
+        } catch (IOException e) {
+            System.out.println("Server can't listen on the specified port");
+        }
+        this.chatServerListener= new ChatServerListener(serverSocket,clients);
+
     }
 
     @Override
     public void run() {
+        System.out.println("Server is running");
+        while (true)
+        {
+            try {
+
+
+        }
+        catch (Exception e)
+        { System.err.println(e);
+        }
+
+        }
         //TODO loop and wait on the serverMessageQ. respond to the messages accordingly.
         // update and maintain the clients map when applicable.
     }
