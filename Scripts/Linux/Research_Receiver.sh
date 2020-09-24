1b1fed9d44728f86bb567bf3d319ebef1c555c20
Siri Khalsa <skhalsa10@live.com>
2020-09-23 23:13:40 -0600

    testing scripts with research version sof server and client and also added a receiver script

--- Scripts/Linux/Research_Receiver.sh
+++ Scripts/Linux/Research_Receiver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'How many total clients(must be a factor of 2) will this research test have? (this sender script will divide the total clients and create client1 through clientTotal/2 and pair them with a client in the receiver half)'
+echo 'How many total clients(must be a factor of 2) will this research test have?(these will be the receiver PART)'
 ##Ask the user running script to privide research numbers
 read clients
 ##build a variable to use that point to the client to run
@@ -10,5 +10,5 @@ printf -v ChatCLI "$(pwd)/../../jars/ChatCLI.jar"
 for (( i=1; i<=$((clients/2)); i++ ))
     do
         ##/usr/bin/java -jar $ChatCLI client${i} localhost 8558 research client$((clients -(2-1)))&
-        /usr/bin/java -jar $ChatCLI client${i} localhost 8558 research client$((clients -(${i}-1)))&
+        /usr/bin/java -jar $ChatCLI client$((clients -(${i}-1))) localhost 8558 research client${i}&
 done
