b600d25474fc167fbdb3fd6e32c8e21a6ac1504e
Siri Khalsa <skhalsa10@live.com>
2020-09-27 20:22:57 -0600

    added the ability to set the path of the java jre in the linux scripts

--- Scripts/Linux/Research_Receiver.sh
+++ Scripts/Linux/Research_Receiver.sh
@@ -3,6 +3,10 @@
 echo 'How many total clients(must be a factor of 2) will this research test have?(these will be the receiver PART)'
 ##Ask the user running script to privide research numbers
 read clients
+
+echo "Enter path to java"
+read path
+
 ##build a variable to use that point to the client to run
 printf -v ChatCLI "$(pwd)/../../jars/ChatCLI.jar"
 ## loop on input number of clients
@@ -10,5 +14,5 @@ printf -v ChatCLI "$(pwd)/../../jars/ChatCLI.jar"
 for (( i=1; i<=$((clients/2)); i++ ))
     do
         ##/usr/bin/java -jar $ChatCLI client${i} localhost 8558 research client$((clients -(2-1)))&
-        /usr/bin/java -jar $ChatCLI client$((clients -(${i}-1))) localhost 8558 research client${i}&
+        ${path} -jar $ChatCLI client$((clients -(${i}-1))) localhost 8558 research client${i}&
 done
--- Scripts/Linux/Research_Sender.sh
+++ Scripts/Linux/Research_Sender.sh
@@ -3,6 +3,10 @@
 echo 'How many total clients(must be a factor of 2) will this research test have? (this sender script will divide the total clients and create client1 through clientTotal/2 and pair them with a client in the receiver half)'
 ##Ask the user running script to privide research numbers
 read clients
+
+echo "Enter path to java"
+read path
+
 ##build a variable to use that point to the client to run
 printf -v ChatCLI "$(pwd)/../../jars/ChatCLI.jar"
 ## loop on input number of clients
@@ -10,5 +14,5 @@ printf -v ChatCLI "$(pwd)/../../jars/ChatCLI.jar"
 for (( i=1; i<=$((clients/2)); i++ ))
     do
         ##/usr/bin/java -jar $ChatCLI client${i} localhost 8558 research client$((clients -(2-1)))&
-        /usr/bin/java -jar $ChatCLI client${i} localhost 8558 research client$((clients -(${i}-1)))&
+        ${path} -jar $ChatCLI client${i} localhost 8558 research client$((clients -(${i}-1)))&
 done
