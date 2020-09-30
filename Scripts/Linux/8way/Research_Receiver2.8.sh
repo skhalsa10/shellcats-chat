87c44af84f3e267c962df8b98ddc10fa290ef343
Siri Khalsa <skhalsa10@live.com>
2020-09-29 21:29:10 -0600

    added scripts for 8 machines

---
+++ Scripts/Linux/8way/Research_Receiver2.8.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo 'How many total clients(must be a factor of 2) will this research test have? (this sender script will divide the total clients and create client1 through clientTotal/2 and pair them with a client in the receiver half)'
+##Ask the user running script to privide research numbers
+read clients
+
+echo "Enter path to java"
+read path
+
+echo "Enter the number of messages a client will send"
+read messages
+
+##build a variable to use that point to the client to run
+printf -v ChatCLI "$(pwd)/../../../jars/ChatCLI.jar"
+## loop on input number of clients
+
+addto=$((clients/8))
+
+
+for (( i=1; i<=$((clients/8)); i++ ))
+    do
+        ##73.42.106.175
+        ##/usr/bin/java -jar $ChatCLI client${i} localhost 8558 research client$((clients -(2-1)))&
+        index=$((${i}+${addto}))
+        echo client$((clients -(${index}-1)))
+        ${path} -jar $ChatCLI client$((clients -(${index}-1))) 73.42.106.175 8558 research client${index} ${messages}&
+done
